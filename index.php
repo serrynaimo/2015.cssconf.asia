0538e256c25f17be048ece000134251e4ffef3ec
Thomas Gorissen <mail@serrynaimo.com>
2015-11-05 10:47:44 +0800

    myrepublic

--- index.php
+++ index.php
@@ -41,12 +41,13 @@
             </a>
             <p class="links">
                 <a href="http://smove.sg" target="_blank"><img src="img/smove.png" style="margin-right: -2px" /></a>
+                <a href="http://clubmate.sg" target="_blank"><img src="img/clubmate.png" style="width: 50px;" /></a>
+                <a href="http://hired.com" target="_blank"><img src="img/hired.png" style="" /></a>
                 <a href="http://tradegecko.com" target="_blank"><img src="img/tradegecko.png" /></a>
                 <a href="http://piktochart.com" target="_blank"><img src="img/piktochart.png" style="" /></a>
-                <a href="http://clubmate.sg" target="_blank"><img src="img/clubmate.png" style="width: 50px;" /></a>
-                <a href="http://meteor.com" target="_blank"><img src="img/meteor.png" style="top: -5px;" /></a>
+                <a href="http://myrepublic.com.sg" target="_blank"><img src="img/myrepublic.png" /></a>
                 <a href="http://redmart.com" target="_blank"><img src="img/redmart.png" style="" /></a>
-                <a href="http://hired.com" target="_blank"><img src="img/hired.png" style="" /></a>
+                <a href="http://meteor.com" target="_blank"><img src="img/meteor.png" style="top: -5px;" /></a>
                 <a href="http://skyscanner.com" target="_blank"><img src="img/skyscanner.png" style="width: 120px;"/></a>
                 <a href="http://opera.com" target="_blank"><img src="img/opera.png" style="width: 85px;" /></a>
                 <a href="http://sonoport.com" target="_blank"><img src="img/sonoport.png" style="" /></a>
